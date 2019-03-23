b940a03d4c7896b21103feabf03200d39497ef57
Lu Jin <lillianjin67@gmail.com>
2019-03-23 18:27:33 -0500

    v0.2.43

--- lujin2_MP2.c
+++ lujin2_MP2.c
@@ -226,15 +226,15 @@ ssize_t mp2_write (struct file *filp, const char __user *buf, size_t count, loff
         switch (buffer[0])
         {
             case REGISTRATION:
-                sscanf(buffer + 2, "%u,%lu,%lu\n", &pid, &period, &process_time);
+                sscanf(buffer + 3, "%u, %lu, %lu\n", &pid, &period, &process_time);
                 mp2_register(pid, period, process_time);
                 break;
             case YIELD:
-                sscanf(buffer + 2, "%u\n", &pid);
+                sscanf(buffer + 3, "%u\n", &pid);
                 break;
             mp2_yield(pid);
             case DEREGISTRATION:
-                sscanf(buffer + 2, "%u\n", &pid);
+                sscanf(buffer + 3, "%u\n", &pid);
                 mp2_deregister(pid);
                 break;
             default:
