b213c5c5d825a3e1e3c485c195d9bc28abcfe39b
Lu Jin <lillianjin67@gmail.com>
2019-03-25 16:55:08 -0500

    0.6.5

--- userapp.c
+++ userapp.c
@@ -128,10 +128,10 @@ int main(int argc, char* argv[]){
     t = 0;
     while(t < num){
         gettimeofday(&start, NULL);
-        printf("job %d is running, start time:\t%llu.%llu s\n", t, (unsigned long long)start.tv_sec, (unsigned long long)start.tv_usec);
+        printf("pid is %u, job %d is running, start time:\t%llu.%llu s\n", pid, t, (unsigned long long)start.tv_sec, (unsigned long long)start.tv_usec);
         do_job(process_time);
         gettimeofday(&end, NULL);
-        printf("job %d finished, end time:    \t%llu.%llu s\n", t, (unsigned long long)end.tv_sec, (unsigned long long)end.tv_usec);
+        printf("pid is %u, job %d finished, end time:    \t%llu.%llu s\n", pid, t, (unsigned long long)end.tv_sec, (unsigned long long)end.tv_usec);
         YIELD(pid);
         t++;
     }
