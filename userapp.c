2102c4e4fb2afbde7970ba266e9fbf0f3d971305
Lu Jin <lillianjin67@gmail.com>
2019-03-25 13:46:00 -0500

    0.5.49

--- userapp.c
+++ userapp.c
@@ -69,13 +69,13 @@ void do_job(unsigned long time){
     struct timeval start, end;
     unsigned long long t0, t1;
     gettimeofday(&start, NULL);
-    t1 = (unsigned long long)start.tv_sec * 1000000 + start.tv_usec;
+    t0 = (unsigned long long)start.tv_sec * 1000000 + start.tv_usec;
 	for(int i=0; i<100000000; i++){
 		for(int j=20; j > 0; j--){
 			res = res * j;
             gettimeofday(&end, NULL);
-            t2 = (unsigned long long)end.tv_sec * 1000000 + end.tv_usec;
-            if(t2 - t1 >= time){
+            t1 = (unsigned long long)end.tv_sec * 1000000 + end.tv_usec;
+            if(t1 - t0 >= time){
                 return;
             }
 		}
@@ -131,7 +131,7 @@ int main(int argc, char* argv[]){
         printf("job %d is running, pid: %u, start time:\t%llu.%llu\n", t, pid, (unsigned long long)start.tv_sec, (unsigned long long)start.tv_usec);
         do_job(process_time);
         gettimeofday(&end, NULL);
-        printf("job %d finished, pid: %u, end time:    \t%llu ms\n", t, pid, (unsigned long long)end.tv_sec, (unsigned long long)start.tv_usec);
+        printf("job %d finished, pid: %u, end time:    \t%llu.%llu\n", t, pid, (unsigned long long)end.tv_sec, (unsigned long long)start.tv_usec);
         YIELD(pid);
         t++;
     }
