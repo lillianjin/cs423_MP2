a4ccf7a8abdc54620e7db88b28372b9f0064b48a
Lu Jin <lillianjin67@gmail.com>
2019-03-25 18:36:56 -0500

    0.6.14

--- userapp.c
+++ userapp.c
@@ -45,6 +45,7 @@ int my_read_status(unsigned int pid){
         perror("Proc file not exists!");
         return 0;
     }
+    printf("Task in /proc/mp2/status are listed: \n", tmp);
     while(1){
         curr = getline(&line, &n, f);
         // printf("curr is %ld\n", curr);
@@ -97,7 +98,7 @@ int main(int argc, char* argv[]){
     int t;
 
     if(argc != 4){
-        perror("Please input the command again in the format of './userapp period process_time(in ms) num_of_jobs'" );
+        perror("Please input the command again in the format of './userapp period process_time(in ms) num_of_jobs'." );
         return 0;
     }
 
@@ -106,7 +107,7 @@ int main(int argc, char* argv[]){
     process_time = strtoul(argv[2], NULL, 10);
     num = atoi(argv[3]);
 
-    printf("pid is %u, period is %u, process_time is %lu, number of jobs is %d\n", pid, period, process_time, num);
+    printf("Read task succeeded: pid is %u, period is %u, process_time is %lu, number of jobs is %d.\n", pid, period, process_time, num);
 
     if(process_time > period){
         perror("Period must be larger than process time");
@@ -116,36 +117,34 @@ int main(int argc, char* argv[]){
     // REGISTERATION
     REGISTER(pid, period, process_time);
     if(my_read_status(pid)==0){
-        printf("Registration failed.\n");
+        printf("Task %u registration failed.\n", pid);
         return 0;
     }
-    printf("Registration succeeded.\n");
+    printf("Task %u registration succeeded.\n", pid);
 
     // YIELD
     // gettimeofday(&init, NULL);
-    printf("Yield begins.\n");
+    printf("Task %u yield begins.\n", pid);
     YIELD(pid);
     t = 0;
     while(t < num){
         gettimeofday(&start, NULL);
-        printf("pid is %u, job %d is running, start time:\t%llu.%llu s\n", pid, t, (unsigned long long)start.tv_sec, (unsigned long long)start.tv_usec);
+        printf("Job %d of task %u is running, start time:\t%llu.%llu s.\n", pid, t, (unsigned long long)start.tv_sec, (unsigned long long)start.tv_usec);
         do_job(process_time);
         gettimeofday(&end, NULL);
-        printf("pid is %u, job %d finished, end time:    \t%llu.%llu s\n", pid, t, (unsigned long long)end.tv_sec, (unsigned long long)end.tv_usec);
+        printf("Job %d of task %u finished, end time:    \t%llu.%llu s.\n", pid, t, (unsigned long long)end.tv_sec, (unsigned long long)end.tv_usec);
         YIELD(pid);
         t++;
     }
-    printf("Yield succeeded.\n");
+    printf("Task %u yield succeeded.\n", pid);
 
     
     // DEREGISTERATION
     DEREGISTER(pid);
     if(my_read_status(pid)==1){
-        printf("Unregistration failed.\n");
-        return 0;
+        printf("Task %u unregistration failed.\n", pid);
     }
-    printf("Unregistration succeeded.\n");
+    printf("Task %u unregistration succeeded.\n", pid);
 
-
-
+    return 0;
 }
